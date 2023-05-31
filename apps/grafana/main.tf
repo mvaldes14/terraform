f5b119e284e271be924d9aacd29302df0c766c48
mvaldes <mvaldes.revilla@gmail.com>
2023-05-31 14:07:30 -0500

    adding atlantis yml

--- apps/grafana/main.tf
+++ apps/grafana/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     grafana = {
-      source = "grafana/grafana"
+      source  = "grafana/grafana"
       version = "1.40.1"
     }
   }
 }
 
 variable "grafana_api_token" {
-    type = string
-  }
+  type = string
+}
