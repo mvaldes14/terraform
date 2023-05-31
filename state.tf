e57670387dd4f98d06defcf5b2f2e7297b3bd00c
mvaldes <mvaldes.revilla@gmail.com>
2023-05-31 15:28:02 -0500

    updating state to hcp

--- state.tf
+++ state.tf
@@ -1,13 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket                      = "terraform"
-    key                         = "terraform.tfstate"
-    region                      = "main"
-    endpoint                    = "https://s3.mvaldes.dev"
-    force_path_style            = true
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_metadata_api_check     = true
+  cloud {
+    organization = "elxilote"
+
+    workspaces {
+      name = "example-workspace"
+    }
   }
 }
-
