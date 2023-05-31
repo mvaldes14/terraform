23a88f8525404eb83dbce761177d4c85a78a4e58
mvaldes <mvaldes.revilla@gmail.com>
2023-05-31 14:18:07 -0500

    removing vars cause ugh

--- state.tf
+++ state.tf
@@ -4,8 +4,6 @@ terraform {
     key                         = "terraform.tfstate"
     region                      = "main"
     endpoint                    = "https://s3.mvaldes.dev"
-    access_key                  = var.access_key
-    secret_key                  = var.secret_key
     force_path_style            = true
     skip_credentials_validation = true
     skip_region_validation      = true
