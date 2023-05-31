462a1e6e40ce12559458c811f5bb1b5b09ebb38f
mvaldes <mvaldes.revilla@gmail.com>
2023-05-31 14:43:24 -0500

    deprecated arguments

--- apps/grafana/datasources.tf
+++ apps/grafana/datasources.tf
@@ -1,15 +1,9 @@
-# resource "grafana_data_source" "loki" {
-#   type = "loki"
-#   url  = "http://loki.local.net"
-#   name = "Loki"
-# }
-
 resource "grafana_data_source" "elasticsearch" {
   type          = "elasticsearch"
   url           = "http://elastic.server.local.net"
   name          = "Elasticsearch"
   database_name = "filebeat-*"
-  json_data {
+  json_data_encoded {
     es_version = "7.10.0"
     time_field = "@timestamp"
   }
@@ -22,9 +16,7 @@ resource "grafana_data_source" "prometheus" {
 }
 
 resource "grafana_data_source" "redis" {
+  name = "redis"
   type = "redis-datasource"
   url  = "redis://192.168.1.22:6379"
-  json_data {
-    client = "standalone"
-  }
 }
