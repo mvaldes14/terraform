ddba449a2c497372933c31358cef416b518ee219
mvaldes <mvaldes.revilla@gmail.com>
2023-09-26 12:47:59 -0500

    initial aws terraform setup

--- apps/aws/main.tf
+++ apps/aws/main.tf
@@ -2,3 +2,7 @@ resource "aws_kms_key" "key" {
   description             = "Key to encrypt and decrypt files in aws"
   deletion_window_in_days = 10
 }
+
+output "key_arn" {
+  value = aws_kms_key.key.arn
+}
