bc3fdbed6ee52ede0c8206f57a11c79841cede12
mvaldes <mvaldes.revilla@gmail.com>
2023-12-02 16:06:36 -0600

    adding flake nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Loads Terraform";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    outputs = { nixpkgs, self}:
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true;};
+      in {
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.terraform
+          ];
+        };
+      };
+}
