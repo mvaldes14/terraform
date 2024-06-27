c914ff72051222944e0aec0209fe1454df71872c
mvaldes <mvaldes.revilla@gmail.com>
2024-06-26 22:02:12 -0500

    feat: TW-37

--- apps/github/main.tf
+++ apps/github/main.tf
@@ -1,34 +1,40 @@
 resource "github_repository" "repo" {
-  for_each = local.repositories
-  name = each.key
-  visibility = each.value.visibility
-  topics = tolist(each.value.topics)
-  license_template = each.value.license
+  for_each             = local.repositories
+  name                 = each.key
+  visibility           = each.value.visibility
+  topics               = tolist(each.value.topics)
+  license_template     = each.value.license
   vulnerability_alerts = true
 }
 
-resource "github_actions_secret" "dockerhub_token"{
- for_each =  toset(local.repo_with_docker)
- repository = each.key
- secret_name = "DOCKERHUB_TOKEN"
- plaintext_value = var.dockerhub_token
+resource "github_actions_secret" "dockerhub_token" {
+  for_each        = toset(local.repo_with_docker)
+  repository      = each.key
+  secret_name     = "DOCKERHUB_TOKEN"
+  plaintext_value = var.dockerhub_token
 }
 
-resource "github_actions_secret" "dockerhub_username"{
- for_each =  toset(local.repo_with_docker)
- repository = each.key
- secret_name = "DOCKERHUB_USERNAME"
- plaintext_value = var.dockerhub_username
+resource "github_actions_secret" "dockerhub_username" {
+  for_each        = toset(local.repo_with_docker)
+  repository      = each.key
+  secret_name     = "DOCKERHUB_USERNAME"
+  plaintext_value = var.dockerhub_username
 }
 
 resource "github_actions_secret" "telegram_chat" {
-  repository = "ansible_playbooks"
-  secret_name = "TELEGRAM_TOKEN"
+  repository      = "ansible_playbooks"
+  secret_name     = "TELEGRAM_TOKEN"
   plaintext_value = var.telegram_token
 }
 
 resource "github_actions_secret" "telegram_to" {
-  repository = "ansible_playbooks"
-  secret_name = "TELEGRAM_TO"
+  repository      = "ansible_playbooks"
+  secret_name     = "TELEGRAM_TO"
   plaintext_value = var.telegram_to
+}
+
+resource "github_actions_secret" "ansible_vault_password" {
+  repository      = "ansible_playbooks"
+  secret_name     = "ANSIBLE_VAULT_PASSWORD"
+  plaintext_value = var.ansible_vault_password
 }
--- apps/github/variables.tf
+++ apps/github/variables.tf
@@ -1,17 +1,20 @@
 variable "dockerhub_token" {
-  type = string 
+  type      = string
   sensitive = true
-} 
-
+}
 variable "dockerhub_username" {
-  type = string 
+  type      = string
   sensitive = true
-} 
+}
 variable "telegram_token" {
-  type = string 
+  type      = string
   sensitive = true
-} 
+}
 variable "telegram_to" {
-  type = string 
+  type      = string
   sensitive = true
-} 
+}
+variable "ansible_vault_password" {
+  type      = string
+  sensitive = true
+}
