5e37a244e1389d9ce64200184f334b8fb5650427
mvaldes <mvaldes.revilla@gmail.com>
2025-05-17 12:56:38 -0500

    updating flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,35 @@
 {
-  description = "Terraform Environment";
+  description = "IaC Flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = {
-    nixpkgs,
-    self,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        terraform
-        tflint
-        tfsec
-        terraform-ls
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgsUnfree = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+        };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgsUnfree; [terraform tfsec tflint terraform-ls];
+          };
+        };
+      };
+    };
 }
