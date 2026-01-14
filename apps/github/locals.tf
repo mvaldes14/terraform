fb568a1b404253f86f9d55953ce721ef69bcdd32
mvaldes <mvaldes.revilla@gmail.com>
2026-01-14 08:02:05 -0600

    feat(gh-repo) wiki for automation

--- apps/github/locals.tf
+++ apps/github/locals.tf
@@ -2,94 +2,87 @@ locals {
   repositories = {
     "meal-notifier" = {
       name       = "meal-notifier"
-      private    = false
       license    = "MIT"
       topics     = ["automation"]
       visibility = "public"
     }
     "k8s-apps" = {
       name       = "k8s-apps"
-      private    = false
       license    = "MIT"
       topics     = ["homelab"]
       visibility = "public"
     }
     "ansible_playbooks" = {
       name       = "ansible_playbooks"
-      private    = false
       license    = "MIT"
       topics     = ["automation"]
       visibility = "public"
     }
     "dotfiles" = {
       name       = "dotfiles"
-      private    = false
       license    = "MIT"
       topics     = ["homelab"]
       visibility = "public"
     }
     "chef" = {
       name       = "chef"
-      private    = false
       license    = "MIT"
       topics     = ["automation"]
       visibility = "public"
     }
     "pulumi" = {
       name       = "pulumi"
-      private    = false
       license    = "MIT"
       topics     = ["automation"]
       visibility = "public"
     }
     "twitch-bot" = {
       name       = "twitch-bot"
-      private    = false
       license    = "MIT"
       topics     = ["homelab"]
       visibility = "public"
     }
     "dotfiles-nix" = {
       name       = "dotfiles-nix"
-      private    = "false"
       license    = "MIT"
       topics     = ["homelab"]
       visibility = "public"
     }
     "linear.nvim" = {
       name       = "linear.nvim"
-      private    = "false"
       license    = "MIT"
       topics     = ["neovim"]
       visibility = "public"
     }
     "todoist.nvim" = {
       name       = "todoist.nvim"
-      private    = "false"
       license    = "MIT"
       topics     = ["neovim"]
       visibility = "public"
     }
     "gh-actions" = {
       name       = "gh-actions"
-      private    = "false"
       license    = "MIT"
       topics     = ["automation"]
       visibility = "public"
     }
     "k8s-lsp" = {
       name       = "k8s-lsp"
-      private    = "false"
       license    = "MIT"
       topics     = ["neovim"]
       visibility = "public"
     }
     "otel-collector" = {
       name       = "otel-collector"
-      private    = "false"
       license    = "MIT"
       topics     = ["homelab"]
       visibility = "public"
+    }
+    "wiki" = {
+      name       = "wiki"
+      license    = "MIT"
+      topics     = ["personal"]
+      visibility = "private"
     }
   }
   repo_with_secrets = toset(["meal-notifier", "twitch-bot"])
